cf68cb31069c645ae86c557603cb2b1762f58d56
JiePrass <renjieprass@gmail.com>
2025-07-07 22:00:40 +0700

    try fix config again

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,21 @@
 import type { NextConfig } from "next";
 import type { RuleSetRule } from "webpack";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   webpack(config) {
-    const fileLoaderRule = config.module.rules.find(
-      (rule: any): rule is RuleSetRule =>
-        typeof rule === "object" &&
-        rule !== null &&
-        "test" in rule &&
-        rule.test instanceof RegExp &&
-        rule.test.test(".svg")
+    // Temukan rule bawaan yang menangani SVG
+    const fileLoaderRule = config.module.rules.find((rule: RuleSetRule) =>
+      rule?.test instanceof RegExp && rule.test.test(".svg")
     );
 
+    // Exclude .svg dari file-loader bawaan
     if (fileLoaderRule) {
       fileLoaderRule.exclude = /\.svg$/i;
     }
 
+    // Tambahkan rule SVGR untuk memproses SVG sebagai React Component
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
